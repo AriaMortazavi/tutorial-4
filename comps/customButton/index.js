9a956dfd19b2c9424ee1877bdba901c828bf8d17
AriaMortazavi <ariamortazavi@ymail.com>
2020-03-31 06:22:08 -0700

    first push

---
+++ comps/customButton/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./cb.css";
+
+const customButton = () => 
+<div className="custom_button_box">
+    <div className="custom_button_box_inner">
+        Button
+    </div>
+</div>
+
+export default customButton;
---
+++ stories/customButton.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import customButton from '../comps/customButton';
+
+export default {
+    title:"customButton",
+    component:customButton,
+};
+
+export const MyCustomButton = () => <customButton />
